390b6c2fc86d771a2d98e70d782e97e93af84e6b
Julian <jll38@njit.edu>
2022-04-29 15:44:44 -0400

    fix

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,4 +1,11 @@
-"""This test the homepage"""
+from flask_login import login_user, login_required, logout_user, current_user
+
+def login_test(client):
+    login_user('johncena@gmail.com')
+    assert current_user.__name == 'johncena@gmail.com'
+    assert current_user.is_authenticated()
+
+    """This test the homepage"""
 
 def test_request_main_menu_links(client):
     """This makes the index page"""
