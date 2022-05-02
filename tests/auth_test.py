f2c16bb91831cb9d78d30f15f78348d47682c8bc
Julian <jll38@njit.edu>
2022-05-01 21:11:37 -0400

    login_test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,8 +1,7 @@
 from flask_login import login_user, login_required, logout_user, current_user
 from app.db import db
 from app.db.models import User, Song
-import app
-
+from flask import current_app
 
 """This test the homepage"""
 
@@ -28,10 +27,11 @@ def test_dashboard_deny(client):
 
 
 def test_dashboard_accept(client):
-    with app.app_context():
-        user = User.query.first()
-        db.session.add(user)
-        db.session.commit()
-        login_user(user)
-        response = client.get("/dashboard")
-        assert response.status_code == 200
+    user = User.query.first()
+    user.authenticated = True
+    db.session.add(user)
+    db.session.commit()
+    login_user(user)
+    response = client.get("/dashboard")
+    assert response.status_code == 200
+    user.authenticated = False
